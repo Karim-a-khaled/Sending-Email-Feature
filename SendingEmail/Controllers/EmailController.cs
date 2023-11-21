ed6316460e6d74b89101d8cc5208052eb365b2d1
karim-a-khaled <karim.a.s.khaled@gmail.com>
2023-11-21 12:31:18 +0300

    Add project files.

---
+++ SendingEmail/Controllers/EmailController.cs
@@ -0,0 +1,29 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using MimeKit.Text;
+
+namespace SendingEmail.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+        private readonly EmailService _emailService;
+
+        public EmailController(EmailService emailService)
+        {
+            _emailService = emailService;
+        }
+
+        [HttpPost]
+        public IActionResult SendEmail(EmailDto request)
+        {
+            
+            _emailService.SenEmail(request);
+            return Ok("An Email Was Sent To: " + request.To);
+        }
+    }
+}
